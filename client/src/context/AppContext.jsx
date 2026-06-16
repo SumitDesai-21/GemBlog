2b396a85d6a187c54da57d079020e72014ec7cdb
SumitDesai-21 <sumit.desai3433@gmail.com>
2026-06-16 08:52:00 +0530

    Modified Appcontext

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -3,7 +3,7 @@ import {createContext, useContext, useEffect, useState} from 'react'
 import {useNavigate} from 'react-router-dom';
 import axios from 'axios'
 import toast from 'react-hot-toast';
-
+    
 axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
 const AppContext = createContext();
